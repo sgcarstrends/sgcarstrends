d20175d536c9ef14bd3eb57f7199a85321ffd4c7
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-05-04 01:35:38 +0800

    Remove Trigger.dev

--- apps/updater/src/trigger/update-coe.ts
+++
@@ -1,5 +0,0 @@
-import { schedulers } from "@updater/config/schedulers";
-import { updateCOE } from "@updater/lib/updateCOE";
-import { createUpdateTask } from "@updater/utils/createUpdateTask";
-
-export const updateCOETask = createUpdateTask("coe", schedulers.coe, updateCOE);
