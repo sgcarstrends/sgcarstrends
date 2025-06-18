04cd8131de21536bc7dd16d453ead77193e2e751
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-06-19 02:02:17 +0800

    Remove unused endpoint

--- apps/api/src/v1/index.ts
+++ apps/api/src/v1/index.ts
@@ -1,6 +1,4 @@
-import { getCarsByFuelType } from "@api/lib/getCarsByFuelType";
 import { OpenAPIHono } from "@hono/zod-openapi";
-import { FuelType } from "@sgcarstrends/types";
 import { bearerAuth } from "hono/bearer-auth";
 import { Resource } from "sst";
 import cars from "./routes/cars";
@@ -10,11 +8,6 @@ import months from "./routes/months";
 const app = new OpenAPIHono();
 
 app.use(bearerAuth({ token: Resource.SG_CARS_TRENDS_API_TOKEN.value }));
-
-app.get("/", async (c) => {
-  const month = c.req.query("month");
-  return c.json({ data: await getCarsByFuelType(FuelType.Petrol, month) });
-});
 
 app.route("/cars", cars);
 app.route("/coe", coe);
