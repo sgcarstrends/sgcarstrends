3569e0c078cbcfa201d031bf61c5cf68a9a7b219
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-07-26 20:53:58 +0800

    chore: update QStash scheduler URL

--- infra/qstash.ts
+++ infra/qstash.ts
@@ -1,10 +1,10 @@
-import { domain } from "./router";
+import { domain, subDomain } from "./router";
 
 const SCHEDULER = "*/60 0-10 * * 1-5";
 
 // QStash Scheduler for updater workflows
 new upstash.QStashScheduleV2("Scheduler", {
-  destination: `https://${domain}/workflows/trigger`,
+  destination: `https://${subDomain("api")}.${domain}/workflows/trigger`,
   forwardHeaders: {
     Authorization: `Bearer ${process.env.SG_CARS_TRENDS_API_TOKEN}`,
   },
