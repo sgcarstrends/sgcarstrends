08f3797d2f2d717e79d99243041d169bb2fa8abc
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-08-31 04:21:10 +0800

    feat: add COE quota premium banner

---
+++ apps/web/src/app/coe/layout.tsx
@@ -0,0 +1,18 @@
+import { QuotaPremiumTicker } from "@web/components/quota-premium-ticker";
+import { getLatestCOEResults } from "@web/utils/cached-api";
+import type { PropsWithChildren } from "react";
+
+interface Props extends PropsWithChildren {}
+
+const Layout = async ({ children }: Props) => {
+  const data = await getLatestCOEResults();
+
+  return (
+    <>
+      <QuotaPremiumTicker data={data} />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
