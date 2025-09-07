d6dbdb47cc242cfc0958096bad14e31a0bc034d8
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-09-07 20:10:36 +0800

    refactor(web): update revalidate API to use native Response

--- apps/web/src/app/api/revalidate/route.ts
+++ apps/web/src/app/api/revalidate/route.ts
@@ -1,15 +1,15 @@
 import { revalidateTag } from "next/cache";
-import { NextRequest, NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export const GET = (req: NextRequest) => {
   const secret = req.nextUrl.searchParams.get("secret");
   if (secret !== process.env.NEXT_PUBLIC_REVALIDATE_TOKEN) {
-    return NextResponse.json({ message: "Invalid token!" }, { status: 401 });
+    return Response.json({ message: "Invalid token!" }, { status: 401 });
   }
 
-  const tags = req.nextUrl.searchParams.get("tags");
-  if (tags) {
-    revalidateTag(tags);
-    return NextResponse.json({ revalidated: true, now: Date.now() });
+  const tag = req.nextUrl.searchParams.get("tag");
+  if (tag) {
+    revalidateTag(tag);
+    return Response.json({ revalidated: true, now: Date.now() });
   }
 };
