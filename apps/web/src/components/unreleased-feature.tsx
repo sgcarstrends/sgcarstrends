e08fcd509b541a2c230d7f651d52e19e14d4d431
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-10-03 12:18:46 +0800

    style(web): minor adjustment to the UnreleasedFeature component

--- apps/web/src/components/unreleased-feature.tsx
+++ apps/web/src/components/unreleased-feature.tsx
@@ -1,7 +1,10 @@
-import type { ReactNode } from "react";
 import { FEATURE_FLAG_UNRELEASED } from "@web/config";
+import type { ReactNode } from "react";
 
-export const UnreleasedFeature = ({ children }: { children: ReactNode }) =>
-  FEATURE_FLAG_UNRELEASED && (
-    <div className="border border-dashed border-red-500 p-1">{children}</div>
+export const UnreleasedFeature = ({ children }: { children: ReactNode }) => {
+  return (
+    FEATURE_FLAG_UNRELEASED && (
+      <div className="outline-dashed outline-2 outline-red-500">{children}</div>
+    )
   );
+};
