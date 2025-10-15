d6b78b30ad9fda98917069347ee9fbaaf26b18a9
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-10-15 13:57:35 +0800

    chore: remove benchmarks

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -1,10 +1,9 @@
-import codspeedPlugin from "@codspeed/vitest-plugin";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { configDefaults, defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react(), codspeedPlugin()],
+  plugins: [tsconfigPaths(), react()],
   test: {
     globals: true,
     coverage: {
