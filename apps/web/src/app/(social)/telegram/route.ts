fd40c4ea5bb31b5e9bd26a392c4ae5c43fd79f42
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-11-01 13:33:29 +0800

    Revert "refactor(web): consolidate social redirects"
    This reverts commit 7234d774c5566d06204f0aefd24f208785b465bc.

---
+++ apps/web/src/app/(social)/telegram/route.ts
@@ -0,0 +1,10 @@
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  const url = new URL("https://t.me/sgcarstrends");
+  url.searchParams.set("utm_source", "sgcarstrends");
+  url.searchParams.set("utm_medium", "social_redirect");
+  url.searchParams.set("utm_campaign", "telegram_profile");
+
+  return NextResponse.redirect(url.toString(), 301);
+};
