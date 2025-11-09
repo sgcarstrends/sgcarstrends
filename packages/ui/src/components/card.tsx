7fb90fe108d51edd4ed557fd33f9d751bfb5bc2b
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-11-10 00:19:06 +0800

    chore: exclude shadcn/ui components from checks

--- packages/ui/src/components/card.tsx
+++ packages/ui/src/components/card.tsx
@@ -1,5 +1,6 @@
+import * as React from "react";
+
 import { cn } from "@sgcarstrends/ui/lib/utils";
-import type * as React from "react";
 
 function Card({ className, ...props }: React.ComponentProps<"div">) {
   return (
