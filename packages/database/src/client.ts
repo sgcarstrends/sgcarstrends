d775f1c58eddf09c11c67c401fb7e650da598751
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-11-11 19:55:19 +0800

    chore(database): update neon connection

--- packages/database/src/client.ts
+++ packages/database/src/client.ts
@@ -6,7 +6,8 @@ import * as schema from "./schema";
 const CACHE_TTL = 24 * 60 * 60; // 24 hours in seconds
 
 const sql = neon(process.env.DATABASE_URL as string);
-export const db = drizzle(sql, {
+export const db = drizzle({
+  client: sql,
   cache: upstashCache({
     url: process.env.UPSTASH_REDIS_REST_URL as string,
     token: process.env.UPSTASH_REDIS_REST_TOKEN as string,
