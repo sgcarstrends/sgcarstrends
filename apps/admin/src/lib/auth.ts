f70d57882263226cef0e2ebf9b2b71566c31f64d
Ru Chern CHONG <ruchern.chong@sgcarstrends.com>
2025-11-15 17:14:19 +0800

    fix(admin): unable to login in local

--- apps/admin/src/lib/auth.ts
+++ apps/admin/src/lib/auth.ts
@@ -2,7 +2,7 @@ import { db } from "@sgcarstrends/database";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
-import { admin, oAuthProxy } from "better-auth/plugins";
+import { admin } from "better-auth/plugins";
 import * as schema from "../db/schema";
 
 export const auth = betterAuth({
@@ -13,14 +13,12 @@ export const auth = betterAuth({
   }),
   plugins: [
     admin(),
-    oAuthProxy(),
     nextCookies(), // Make sure this is the last plugin in the array
   ],
   socialProviders: {
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      redirectURI: `${process.env.VERCEL_PROJECT_PRODUCTION_URL}/api/auth/callback/google`,
     },
   },
 });
